49a48b850c1a399c18d78a6edcc8368de77222fe
chengzi-77 <2017592443@qq.com>
2026-07-05 18:03:12 +0800

    Initial commit

---
+++ fooddrink-chengzi-77-main/FoodDrinkApp/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodDrinkApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+		Routing.RegisterRoute(nameof(AddItemPage), typeof(AddItemPage));
+		Routing.RegisterRoute(nameof(FoodDetailPage), typeof(FoodDetailPage));
+	}
+}
---
+++ fooddrink-chengzi-77-main/FoodDrinkApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace FoodDrinkApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
